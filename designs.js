59d68476f9866d2a05e62b4ac6562908a44c022b
BiplabKarki333 <biplabkarki333@gmail.com>
2020-10-28 01:33:52 +0545

    modified the js code to create table instead of divs and the reset button now works

--- designs.js
+++ designs.js
@@ -1,5 +1,9 @@
 
-const gridCode = "<div class = 'thegrid'></div>";
+//const tableCodeStart = "<table>";
+//const tableCodeEnd = "</table>";
+//const gridLengthCodeStart = "<tr>";
+//const gridLengthCodeEnd = '</tr>';
+//const gridWidthCode = "<td></td>";
 
 function checkColor (item, color){
   if (item.style.backgroundColor=== 'white') {
@@ -14,16 +18,30 @@ function makeGrid() {
   let width = document.getElementById('inputWidth');
   let height = document.getElementById('inputHeight');
   let color = document.getElementById('colorPicker');
-  document.getElementById('unimp').innerHTML = gridCode.repeat(width.value)+ "<br>";
-  let i;
-  for (i = 1; i < height.value ; i++) {
-    document.getElementById('unimp').innerHTML += gridCode.repeat(width.value) + "<br>";
-  }
-  document.querySelectorAll('.thegrid').forEach(item => {
-  item.addEventListener('mousedown', event => checkColor(item, color.value))});
-document.getElementById('unimp').innerHTML += "<button type = 'button' onclick='reSet()' id = 'resetButton'>Reset</button>";
+  document.getElementById('unimp').innerHTML = '<table>'+('<tr>' +('<td></td>'.repeat(width.value))+'</tr>').repeat(height.value)+'</table>'+ '<button id = resetButton onclick = reSet()>Reset</button>';
+  //for (i = 0; i<height.value; i++){
+  //document.getElementById('unimp').innerHTML += '<tr>';
+  //document.getElementById('unimp').innerHTML += '<td></td>'.repeat(width.value);
+  //document.getElementById('unimp').innerHTML += '</tr>';
+  //document.getElementById('unimp').innerHTML += '<br>';
+//  }
+//  document.getElementById('unimp').innerHTML += '</table>';
+  //document.querySelectorAll('.thegrid').forEach(item => {
+  //item.addEventListener('mousedown', event => checkColor(item, color.value))});
+//document.getElementById('unimp').innerHTML += "<button type = 'button' onclick='reSet()' id = 'resetButton'>Reset</button>";
+document.querySelectorAll('td').forEach(item => {
+  item.addEventListener('click', event =>{ if (item.style.backgroundColor == 'white'){
+      item.style.backgroundColor = color.value;
+    }
+    else {
+      item.style.backgroundColor = 'white';
+    }
+});
+});
+
 
 };
+
 function reSet() {
   document.getElementById('unimp').innerHTML = "<div id = 'unimp'>\
 <h2>Choose Grid Size</h2>\
