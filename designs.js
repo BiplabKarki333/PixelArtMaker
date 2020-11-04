cc9edee026beaacebef99051a5c2e8ee58d7366a
BiplabKarki333 <biplabkarki333@gmail.com>
2020-11-05 02:05:28 +0545

    removed the incompatibel download fucntion

--- designs.js
+++ designs.js
@@ -1,13 +1,13 @@
 
-
 // When size is submitted by the user, call makeGrid()
 function makeGrid() {
+
   //gets the values of width, height and color as set by the user at the time of clicking submit
   let width = document.getElementById('inputWidth');
   let height = document.getElementById('inputHeight');
   let color = document.getElementById('colorPicker');
   //creates table with the number of rows and columns decided by user
-  document.getElementById('unimp').innerHTML = '<table>'+('<tr>' +('<td></td>'.repeat(width.value))+'</tr>').repeat(height.value)+'</table>'+ '<button id = resetButton onclick = reSet()>Reset</button>'+'<button id = resetButton onclick = clearGrid()>Submit</button>';
+  document.getElementById('unimp').innerHTML = '<table id ="myTable">'+('<tr>' +('<td></td>'.repeat(width.value))+'</tr>').repeat(height.value)+'</table>'+ '<button class = resetButton onclick = reSet()>Reset</button>'+'<button class = resetButton onclick = clearGrid()>Submit</button>';
 //sets the color of all pixels to white
 document.querySelectorAll('td').forEach(item => {
   item.style.backgroundColor = 'white';
@@ -33,20 +33,9 @@ function clearGrid(){
     item.style.backgroundColor = 'white';
 }
 )};
-//function to take user back to home screen where users can choose the grid size again
+
+
+//function to take user back to home screen where users can choose new grid size again
 function reSet() {
-  document.getElementById('unimp').innerHTML = "<div id = 'unimp'>\
-<h2>Choose Grid Size</h2>\
-  <form id='sizePicker'>\
-      Grid Height:\
-      <input type='number' id='inputHeight' name='height' min='1' value='1'>\
-      Grid Width:\
-      <input type='number' id='inputWidth' name='width' min='1' value='1'>\
-      <input type='submit' onclick='makeGrid()'>\
-  </form>\
-  <h2>Pick A Color</h2>\
-  <input type='color' id='colorPicker'>\
-  <h2>Design Canvas</h2>\
-  <table id='pixelCanvas'></table>\
-</div>";
+  location.reload();
 };
